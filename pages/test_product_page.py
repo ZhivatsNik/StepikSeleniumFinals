f10bb19c36cc88d2150207fec1020799b0b599d2
ZhivatsNik <zhivats@gmail.com>
2019-07-03 16:57:33 +0300

    rewrote files main_page.py base_page.py test_main_page.py from begging

--- pages/test_product_page.py
+++
@@ -1,8 +0,0 @@
-from .product_page import ProductPage
-from .main_page import MainPage
-
-def user_can_click_login_button(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = MainPage(browser, link)
-    page.open()
-    page.add_to_basket_button_click()
